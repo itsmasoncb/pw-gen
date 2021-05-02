6e25edfd89783e436abc726cb5b76dbb035a7c8a
Mason <51417315+lookitsmason@users.noreply.github.com>
2021-05-02 19:43:00 -0400

    errror message addition

--- password.py
+++ password.py
@@ -1,17 +1,18 @@
 import string
 import random
+import time
+
 # * key means all, so in this case all from the random module
 # random module generates pseudo-random numbers, built-in to python
-
-password_length = int(input("Enter password length between 8 and 24 characters:"))
-# A prompt and input asking the user to pick
-characters = string.ascii_letters + string.digits + string.punctuation
-# string module constants put together for possible chars in a password string
-
 while True:
-    if password_length not in range(8,24):
-        print("Please use a number between 8 and 24:")
-        break
+    password_length = int(input("Enter password length between 8 and 24 characters:"))
+    # A prompt and input asking the user to pick
+    characters = string.ascii_letters + string.digits + string.punctuation
+    # string module constants put together for possible chars in a password string
+
+    if password_length not in range(8,25):
+        print("\nPlease enter a number between 8 and 24.\n")
+        time.sleep(.25)
 
     else:
         password = []
@@ -20,7 +21,13 @@ while True:
                 password.append(random.choice(characters))
 
         print(''.join(password))
+        time.sleep(.25)
 
-    new_password = input("Make another password? (y/n): ")
-    if new_password.lower() != "y":
-        break
+    if password_length not in range(8,25):    
+        new_password = input("Try Again? (y/n): ")
+        if new_password.lower() != "y":
+            break
+    else:
+        new_password = input("Make another password? (y/n): ")
+        if new_password.lower() != "y":
+            break
